2aa533b536a82a124296951fcde2a1be6aff6c09
douglas-front <douglasjknw007@gmail.com>
2024-07-17 19:10:32 -0300

    fix: bug cors fixed

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
